5f95b77ff3530698b144ffa9b73b9fd161f529b8
aschie <schmc444@gmail.com>
2025-07-23 13:46:04 -0600

    Refactor main.cpp to integrate RF24 radio functionality and update platformio.ini with library dependencies

--- RF_ESP32-C3/RF_ESP32-C3/src/main.cpp
+++ RF_ESP32-C3/RF_ESP32-C3/src/main.cpp
@@ -1,18 +1,43 @@
-#include <Arduino.h>
+#include <SPI.h>
+#include <nRF24L01.h>
+#include <RF24.h>
 
-// put function declarations here:
-int myFunction(int, int);
+RF24 radio(2, 3); // CE, CSN pins
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  Serial.begin(115200);
+
+  if (!radio.begin()) {
+    Serial.println("Radio init failed");
+    while (1);
+  }
+
+  radio.setAutoAck(false);
+  radio.setPALevel(RF24_PA_MAX);
+  radio.setDataRate(RF24_1MBPS);
+  radio.setChannel(76); // Try different channels (0-125)
+  radio.enableDynamicPayloads(); // <-- Add this line
+  radio.openReadingPipe(0, 0xF0F0F0F0E1LL); // Use a non-zero 5-byte address
+  radio.startListening();
+
+  Serial.println("Listening for signals...");
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
+  if (radio.available()) {
+    uint8_t len = radio.getDynamicPayloadSize();
+    if (len > 0 && len <= 32) {
+      uint8_t data[32];
+      radio.read(&data, len);
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+      Serial.print("Received data (len ");
+      Serial.print(len);
+      Serial.print("): ");
+      for (int i = 0; i < len; i++) {
+        Serial.print(data[i], HEX);
+        Serial.print(" ");
+      }
+      Serial.println();
+    }
+  }
 }
